c60e5e3c6ae791e166e5be58cea545d54a987678
Aaron Woo <awoo@chapman.edu>
2021-09-06 13:26:46 -0700

    in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -50,14 +50,14 @@ deck = _.shuffle(deck);
     - add the first two cards in deck to player's hand
     - delete those cards from deck
 */
-for (var playerNum = 0; playerNum < players.length; playerNum++) {
-  for (var cardInDeck = 0; cardInDeck < 2; cardInDeck++) {
-    players[playerNum].hand.push(deck[cardInDeck]);
-  }
-  deck.splice(0, 2);
-}
+// for (var playerNum = 0; playerNum < players.length; playerNum++) {
+//   for (var cardInDeck = 0; cardInDeck < 2; cardInDeck++) {
+//     players[playerNum].hand.push(deck[cardInDeck]);
+//   }
+//   deck.splice(0, 2);
+// }
 
-console.log(players);
+// console.log(players);
 
 // Find Player with highest score
 /*
@@ -78,27 +78,61 @@ console.log(players);
   - return winning player
 */
 
-function winner(players) {
+// function winner(players) {
+//   var highestHand = 0;
+//   var winningPlayer;
+//   for (var playerNum = 0; playerNum < players.length; playerNum++) {
+//     var currentPlayerHand = 0;
+//     for (var cardNum = 0; cardNum < players[playerNum].hand.length; cardNum++) {
+//       if (players[playerNum].hand[cardNum].rank === 'Jack' || players[playerNum].hand[cardNum].rank === 'Queen' ||
+//       players[playerNum].hand[cardNum].rank === 'King') {
+//         currentPlayerHand += 10;
+//       } else if (players[playerNum].hand[cardNum].rank === 'Ace') {
+//         currentPlayerHand += 11;
+//       } else {
+//         currentPlayerHand += players[playerNum].hand[cardNum].rank;
+//       }
+//     }
+//     if (currentPlayerHand > highestHand) {
+//       highestHand = currentPlayerHand;
+//       winningPlayer = players[playerNum];
+//     }
+//   }
+//   return winningPlayer;
+// }
+
+// console.log(winner(players));
+// Generalize so any number of cards can be dealt
+
+function runGame(playersArr, numCards) {
+  for (var players = 0; players < playersArr.length; players++) {
+    for (var cardsDealt = 0; cardsDealt < numCards; cardsDealt++) {
+      playersArr[players].hand.push(deck[cardsDealt]);
+    }
+    deck.splice(0, numCards);
+  }
+
   var highestHand = 0;
   var winningPlayer;
-  for (var playerNum = 0; playerNum < players.length; playerNum++) {
+  for (var playerNum = 0; playerNum < playersArr.length; playerNum++) {
     var currentPlayerHand = 0;
-    for (var cardNum = 0; cardNum < players[playerNum].hand.length; cardNum++) {
-      if (players[playerNum].hand[cardNum].rank === 'Jack' || players[playerNum].hand[cardNum].rank === 'Queen' ||
-      players[playerNum].hand[cardNum].rank === 'King') {
+    for (var cardNum = 0; cardNum < playersArr[playerNum].hand.length; cardNum++) {
+      if (playersArr[playerNum].hand[cardNum].rank === 'Jack' || playersArr[playerNum].hand[cardNum].rank === 'Queen' ||
+        playersArr[playerNum].hand[cardNum].rank === 'King') {
         currentPlayerHand += 10;
-      } else if (players[playerNum].hand[cardNum].rank === 'Ace') {
+      } else if (playersArr[playerNum].hand[cardNum].rank === 'Ace') {
         currentPlayerHand += 11;
       } else {
-        currentPlayerHand += players[playerNum].hand[cardNum].rank;
+        currentPlayerHand += playersArr[playerNum].hand[cardNum].rank;
       }
     }
     if (currentPlayerHand > highestHand) {
       highestHand = currentPlayerHand;
-      winningPlayer = players[playerNum];
+      winningPlayer = playersArr[playerNum];
     }
   }
   return winningPlayer;
 }
-
-winner(players);
+console.log('deck before:', deck);
+console.log(runGame(players, 4));
+console.log('deck after:', deck);
