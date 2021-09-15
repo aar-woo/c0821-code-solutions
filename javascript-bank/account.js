6c924e807af27bc203efb1f94645e4374e8b3e69
Aaron Woo <awoo@chapman.edu>
2021-09-15 10:59:12 -0700

    Completed javascript-bank challenge.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -7,11 +7,32 @@ function Account(number, holder) {
 
 Account.prototype.deposit = function (amount) {
   var depositTransaction = new Transaction('deposit', amount);
-  if (amount > 0 && typeof amount === 'number' && Math.floor(amount) === amount) {
+  if (amount > 0 && Math.floor(amount) === amount) {
     this.transactions.push(depositTransaction);
     return true;
   } else {
     return false;
   }
-}
-;
+};
+
+Account.prototype.withdraw = function (amount) {
+  var withdrawTransaction = new Transaction('withdrawal', amount);
+  if (amount > 0 && Math.floor(amount) === amount) {
+    this.transactions.push(withdrawTransaction);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var balance = 0;
+  for (var numTransactions = 0; numTransactions < this.transactions.length; numTransactions++) {
+    if (this.transactions[numTransactions].type === 'deposit') {
+      balance += this.transactions[numTransactions].amount;
+    } else {
+      balance -= this.transactions[numTransactions].amount;
+    }
+  }
+  return balance;
+};
