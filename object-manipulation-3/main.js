aa15184ed42d71b2708371d465f1a10c0ab6fbd1
Aaron Woo <awoo@chapman.edu>
2021-09-28 19:57:03 +0000

    Completed object-manipulation-3.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,6 +1,5 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-// Players
 var players = [
   {
     name: 'Ken',
@@ -19,7 +18,7 @@ var players = [
     hand: []
   }
 ];
-// create card function
+
 function createCard(cardRank, cardSuit) {
   var card = {
     suit: cardSuit,
@@ -27,84 +26,25 @@ function createCard(cardRank, cardSuit) {
   };
   return card;
 }
-
-// deck creation
-var deck = [];
-var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-var faces = ['Jack', 'Queen', 'King', 'Ace'];
-for (var i = 0; i < suits.length; i++) {
-  for (var val = 2; val <= 10; val++) {
-    deck.push(createCard(val, suits[i]));
-  }
-  for (var faceCards = 0; faceCards < 4; faceCards++) {
-    deck.push(createCard(faces[faceCards], suits[i]));
-  }
-}
-
-// shuffle deck
-deck = _.shuffle(deck);
-
-// deal cards
-/*
-  - for each player
-    - add the first two cards in deck to player's hand
-    - delete those cards from deck
-*/
-// for (var playerNum = 0; playerNum < players.length; playerNum++) {
-//   for (var cardInDeck = 0; cardInDeck < 2; cardInDeck++) {
-//     players[playerNum].hand.push(deck[cardInDeck]);
-//   }
-//   deck.splice(0, 2);
-// }
-
-// console.log(players);
-
-// Find Player with highest score
-/*
-  - highestHand
-  - winning player
-  - look at each player in players
-    - create storage for hand value set to 0
-    - look at each card in player hand
-      - if card.rank is equal to jack, queen or king,
-        - add 10 to hand value
-      - else if card.rank is equal to ace
-        - add 11 to hand value
-      - otherwise
-        - add card.rank to hand value
-    - if hand value is greater than highest hand
-      - set highest hand equal to current hand value
-      - set winning player to current player
-  - return winning player
-*/
-
-function winner(players) {
-  var highestHand = 0;
-  var winningPlayer;
-  for (var playerNum = 0; playerNum < players.length; playerNum++) {
-    var currentPlayerHand = 0;
-    for (var cardNum = 0; cardNum < players[playerNum].hand.length; cardNum++) {
-      if (players[playerNum].hand[cardNum].rank === 'Jack' || players[playerNum].hand[cardNum].rank === 'Queen' ||
-      players[playerNum].hand[cardNum].rank === 'King') {
-        currentPlayerHand += 10;
-      } else if (players[playerNum].hand[cardNum].rank === 'Ace') {
-        currentPlayerHand += 11;
-      } else {
-        currentPlayerHand += players[playerNum].hand[cardNum].rank;
-      }
+function createDeck() {
+  var deck = [];
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  var faces = ['Jack', 'Queen', 'King', 'Ace'];
+  for (var i = 0; i < suits.length; i++) {
+    for (var val = 2; val <= 10; val++) {
+      deck.push(createCard(val, suits[i]));
     }
-    if (currentPlayerHand > highestHand) {
-      highestHand = currentPlayerHand;
-      winningPlayer = players[playerNum];
+    for (var faceCards = 0; faceCards < 4; faceCards++) {
+      deck.push(createCard(faces[faceCards], suits[i]));
     }
   }
-  return winningPlayer;
+  return deck;
 }
-
-// console.log(winner(players));
-// Generalize so any number of cards can be dealt
 
 function runGame(playersArr, numCards) {
+  var deck = createDeck();
+  deck = _.shuffle(deck);
+
   for (var players = 0; players < playersArr.length; players++) {
     for (var cardsDealt = 0; cardsDealt < numCards; cardsDealt++) {
       playersArr[players].hand.push(deck[cardsDealt]);
@@ -113,9 +53,6 @@ function runGame(playersArr, numCards) {
   }
 
   var highestHand = 0;
-  var allHands = {};
-  var tieValue = 0;
-  var tiedPlayers = [];
   var winningPlayer;
   for (var playerNum = 0; playerNum < playersArr.length; playerNum++) {
     var currentPlayerHand = 0;
@@ -135,32 +72,8 @@ function runGame(playersArr, numCards) {
       winningPlayer = playersArr[playerNum];
     }
   }
-  for (var i = 0; i < playersArr.length; i++) {
 
-    if (playersArr[i].playerHand === highestHand &&
-       playersArr[i].name !== winningPlayer.name) {
-      tiedPlayers.push(playersArr[i]);
-    }
-  }
-  console.log('winner before tie run', winningPlayer);
-  if (tiedPlayers.length !== 0) {
-    tiedPlayers.push(winningPlayer);
-    console.log('tied players', tiedPlayers);
-    console.log('tie run');
-    for (var tiedPlayer = 0; tiedPlayer < tiedPlayers.length; tiedPlayer++) {
-      console.log('player:', tiedPlayers[tiedPlayer]);
-      tiedPlayers[tiedPlayer].playerHand = 0;
-      tiedPlayers[tiedPlayer].hand = [];
-      console.log('player after hand clear', tiedPlayers[tiedPlayer]);
-
-    }
-    runGame(tiedPlayers, numCards);
-  }
-
-  console.log('playersArr', playersArr);
-  // console.log('tied players', tiedPlayers);
-  return winningPlayer;
+  console.log('Winner:', winningPlayer.name, winningPlayer);
 }
 
-// console.log('deck before:', deck);
-console.log(runGame(players, 2));
+runGame(players, 2);
