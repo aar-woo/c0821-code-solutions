66b0df4468307ebc944d40ac4c8f55e69b20ba3a
Aaron Woo <awoo@chapman.edu>
2021-10-07 17:47:04 +0000

    Edited readNote function, took out outer for loop and conditional and just used for in loop to read notes to console.

--- node-notes-cli/readNote.js
+++ node-notes-cli/readNote.js
@@ -1,13 +1,10 @@
 const data = require('./data.json');
 
 function readNote() {
-  for (let i = 1; i < data.nextId; i++) {
-    for (const note in data.notes) {
-      if (parseInt(note) === i) {
-        console.log(`${note}: ${data.notes[note]}`);
-      }
-    }
+  for (const note in data.notes) {
+    console.log(`${note}: ${data.notes[note]}`);
   }
+
 }
 
 module.exports = readNote;
