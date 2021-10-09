fe55dfb5a74d292e918e7bbdde4b133f49c13bda
Aaron Woo <awoo@chapman.edu>
2021-10-09 17:58:24 +0000

    Changed GET list of notes to return a JSON array rather than JSON object.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,9 +3,17 @@ const app = express();
 const data = require('./data.json');
 
 app.get('/api/notes', function (req, res) {
+  const notesArr = [];
+  for (const note in data.notes) {
+    notesArr.push(data.notes[note]);
+  }
   res.status(200);
-  res.json(data);
+  res.json(notesArr);
 });
+
+// app.get('/api/notes/:id', function (req, res) {
+
+// });
 
 app.listen(3000, () => {
   // console.log('Port 3000 listening');
