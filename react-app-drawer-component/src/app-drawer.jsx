530d31b987a04ba3f5daf88a9f579a7d1fedb875
Aaron Woo <awoo@chapman.edu>
2021-10-22 18:57:16 +0000

    Completed react-app-drawer-component challenge.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,7 +1,45 @@
-/*
-Components:
-  App drawer button
-  App drawer container
-    Menu headers
-  Shade
-*/
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    let drawerClass;
+    let shadeClass;
+    let shadeOnClick;
+    if (this.state.menuOpen) {
+      drawerClass = 'app-drawer show-menu';
+      shadeClass = 'overlay';
+      shadeOnClick = this.handleClick;
+    } else {
+      drawerClass = 'app-drawer hide-menu';
+      shadeClass = 'overlay hide-shade';
+    }
+    return (
+        <div>
+          <div className={shadeClass} onClick={shadeOnClick}>
+          </div>
+          <i className="fas fa-bars fa-2x" onClick={this.handleClick}></i>
+          <div className={ drawerClass}>
+            <h1>Menu</h1>
+            <ul onClick={this.handleClick}>
+              <li>About</li>
+              <li>Get Started</li>
+              <li>Sign In</li>
+            </ul>
+          </div>
+        </div>
+    );
+  }
+
+  handleClick(event) {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
+}
+
+export default AppDrawer;
