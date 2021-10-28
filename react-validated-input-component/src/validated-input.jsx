d9c85cf3325cfa7fc517acea5b5adec33c03b812
Aaron Woo <awoo@chapman.edu>
2021-10-28 00:20:30 +0000

    Completed react-validated-input-component challenge.

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -13,20 +13,27 @@ class ValidatedInput extends React.Component {
 
   render() {
     let errMessage;
+    let iconClass = 'fas fa-times-circle';
+    const regex = /(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/;
+
     if (this.state.password.length === 0) {
       errMessage = 'A password is required';
     } else if (this.state.password.length < 8) {
       errMessage = 'Your password is too short';
+    } else if (!this.state.password.match(regex)) {
+      errMessage = 'Password must contain a number, capital letter, and special character.';
     } else {
       errMessage = '';
+      iconClass = 'fas fa-check-circle';
     }
+
     return (
     <form>
       <label>
         <p>Password</p>
-        <input type='password' name="password" value={this.state.password} onChange={this.handleChange}></input>
+          <input type='password' name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}" value={this.state.password} onChange={this.handleChange}></input>
       </label>
-        <i className="fas fa-times-circle"></i>
+        <i className={iconClass}></i>
         <p className="no-password">{errMessage}</p>
     </form>
     );
