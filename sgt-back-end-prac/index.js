b79e81159298a6f142feda50b3bda2f1caae8153
Aaron Woo <awoo@chapman.edu>
2022-07-01 16:36:54 -0700

    Added post route that inserts a new grade into the database, given the request is valid.

--- sgt-back-end-prac/index.js
+++ sgt-back-end-prac/index.js
@@ -12,8 +12,8 @@ app.get('/api/grades', async (req, res, next) => {
   let grades;
 
   try {
-    const queryResults = await db.query(sql);
-    grades = queryResults.rows;
+    const queryResponse = await db.query(sql);
+    grades = queryResponse.rows;
     res.status(200).json(grades);
   } catch (err) {
     res.status(500).json({
@@ -21,6 +21,37 @@ app.get('/api/grades', async (req, res, next) => {
     });
   }
 
+});
+
+app.use(express.json());
+
+app.post('/api/grades', async (req, res, next) => {
+  const { name, course, score } = req.body;
+  if (score < 0 || score > 100 || !score || !course || !name) {
+    res.status(400).json({
+      error: 'Invalid request, must contain name, course, and a score between 0 and 100'
+    });
+  }
+
+  const sql = `
+        INSERT into "grades" ("name", "course", "score")
+        values ($1, $2, $3)
+        returning *;
+    `;
+
+  const values = [name, course, score];
+
+  try {
+    const queryResponse = await db.query(sql, values);
+    const insertedGrade = queryResponse.rows[0];
+    res.status(201).json({
+      insertedGrade
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: 'Sorry an error occurred while querying the database'
+    });
+  }
 });
 
 app.listen(3000, () => {
