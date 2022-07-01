a6de7f620a98a4cd61f6348cc828a325fe5b8e35
Aaron Woo <awoo@chapman.edu>
2022-07-01 16:05:17 -0700

    Added get route for /api/grades that queries the grades table and returns all rows.

--- sgt-back-end-prac/index.js
+++ sgt-back-end-prac/index.js
@@ -1,10 +1,28 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const pg = require('pg');
-
 const db = new pg.Pool({
-    connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: 'postgres://localhost/studentGradeTable'
+});
+
+app.get('/api/grades', async (req, res, next) => {
+  const sql = `
+        SELECT * from "grades";
+    `;
+  let grades;
+
+  try {
+    const queryResults = await db.query(sql);
+    grades = queryResults.rows;
+    res.status(200).json(grades);
+  } catch (err) {
+    res.status(500).json({
+      error: 'Sorry an error occurred while querying the database'
+    });
+  }
+
+});
+
+app.listen(3000, () => {
+  // console.log('Listening on port 3000')
 });
