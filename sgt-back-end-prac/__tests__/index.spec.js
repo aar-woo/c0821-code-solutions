e132072d687261920834b5c79bd3dbfdbf64302b
Aaron Woo <awoo@chapman.edu>
2022-07-03 13:12:40 -0700

    Seperated server and app files to allow for testing with Jest and added a test for the root path.

---
+++ sgt-back-end-prac/__tests__/index.spec.js
@@ -0,0 +1,11 @@
+var request = require("supertest");
+var app = require('../index');
+
+describe('Test the root path', () => {
+    test('It should respond to the GET method', async () => {
+      const res = await request(app).get("/");
+  
+      expect(res.statusCode).toBe(200);
+    });
+  });
+  
--- sgt-back-end-prac/index.js
+++ sgt-back-end-prac/index.js
@@ -5,6 +5,10 @@ const db = new pg.Pool({
   connectionString: 'postgres://localhost/studentGradeTable'
 });
 
+app.get('/', async (req, res) => {
+    res.status(200).json("Hello World!");
+})
+
 app.get('/api/grades', async (req, res, next) => {
   const sql = `
         SELECT * from "grades";
@@ -127,6 +131,10 @@ app.delete('/api/grades/:gradeId', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  // console.log('Listening on port 3000')
-});
+app.set('port', 3000);
+
+module.exports = app;
+
+// app.listen(3000, () => {
+//   // console.log('Listening on port 3000')
+// });
